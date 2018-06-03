2d5323cbcaad382d5d9f8aaf91e995d7842eb865
ldstreet <ldstreet@me.com>
2018-06-03 00:28:08 -0500

    add comments to TemplatedModel

--- Sources/TemplateKit/Tag/TemplatedModel.swift
+++ Sources/TemplateKit/Tag/TemplatedModel.swift
@@ -1,6 +1,14 @@
-
+/// Embeds rendered views for `Templated` or `TemplatePathed` models
+///
+///     templatedModel(<model>)
+///
+/// The model must either be `Templated` or `TemplatePathed`
 public final class TemplatedModel: TagRenderer {
     
+    /// Creates a new `TemplatedModel` tag renderer.
+    public init() {}
+    
+    /// See `TagRenderer`.
     public func render(tag: TagContext) throws -> EventLoopFuture<TemplateData> {
         
         try tag.requireParameterCount(1)
@@ -10,14 +18,15 @@ public final class TemplatedModel: TagRenderer {
         let renderer = try tag.container.make(TemplateRenderer.self)
         
         let view: EventLoopFuture<View>
-        if let templatePath = context.dictionary?["templatePath"]?.string {
+        if let templatePath = context.dictionary?["templatePath"]?.string { // `TemplatePathed`
             view = renderer.render(templatePath, context)
-        } else if let template = context.dictionary?["template"]?.data {
+        } else if let template = context.dictionary?["template"]?.data { // `Templateed`
             view = renderer.render(template: template, context)
         } else {
             throw(TemplateKitError(identifier: "notRenderable", reason: "First paramater must be Templated or TemplatePathed. The data supplied does not contain a templatePath or template"))
         }
         
+        /// Convert into TemplateData
         return view.map { view -> TemplateData in
             return .data(view.data)
         }
